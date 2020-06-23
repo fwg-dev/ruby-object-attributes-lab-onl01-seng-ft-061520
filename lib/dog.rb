560e6d6e2183a35764057e98256a9cdd7a4177dd
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-23 02:45:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,7 @@
 class Dog
 end 
+# Dog is defined within lib/dog.rb
+# Dog with names #name= writes the name of the dog to an instance variable @name
+# Dog with names #name reads the name of the dog from an instance variable @name
+# Dog with breeds #breed= writes the breed of the dog to an instance variable @breed
+# Dog with breeds #breed reads the breed of the dog from an instance variable @breed
