4e7b043dddcf20a2fff3c29b9de4c66b13e76a0a
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-23 03:02:03 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+
+# Person with names #name= writes the name of the person to an instance variable @name
+# Person with names #name reads the name of the person from an instance variable @name
+# Person with jobs #job= writes the job of the person to an instance variable @job
+# Person with jobs #job reads the job of the person from an instance variable @job
