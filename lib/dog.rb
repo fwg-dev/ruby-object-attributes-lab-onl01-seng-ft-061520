e4cd4d4b17fa502850f240c75e484d61f54bb3c2
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-23 02:54:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,7 @@ class Dog
   def name=(name)
     @name =name
   end
-  def name 
-    name 
-  end 
+
 end
 # Dog is defined within lib/dog.rb
 # Dog with names #name= writes the name of the dog to an instance variable @name
